093b13d3bd01322b3f07366a58f06cb10da7a804
heeeman <ckalt@hsr.ch>
2017-09-20 21:54:01 +0200

    style change; button onclick listener

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-var source   = document.getElementById("note-template").innerHTML;
-var template = Handlebars.compile(source);
-var context =
-    {notes: [
+
+setContent("note-template", loadNotes())
+
+function setContent(htmlTemplate, context) {
+
+    let source   = document.getElementById(htmlTemplate).innerHTML;
+    let template = Handlebars.compile(source);
+    let html    = template(context);
+    document.getElementById("entryPoint").innerHTML = html;
+}
+
+function loadNotes() {
+  console.log('tue so als ob ich was vom Server lade');
+    return     {notes: [
         {title: "My New Post",
             issueDate: "12.3.2017",
             dueDate:  "morgen",
@@ -25,9 +35,38 @@ var context =
             priority: "",
             finished: "",
             id:3}]};
-var html    = template(context);
+}
 
+function changeStyle(filename) {
+    document.getElementById('baseStyleSheet').href = filename + '.css';
+}
 
-console.log(html);
-document.getElementById("entryPoint").innerHTML = html;
+function sortNotesOnFinishDate() {
+    console.log('sort Notes on finish date -> give me soul');
+    setContent("note-template", {notes: [
 
+        {title: "Rasen mähen",
+            issueDate: "12.01.2017",
+            dueDate:  "12.9.2017",
+            description: "Unbedingt alle Flächen. Die Randsteine nicht vergessen." +
+            "und \n endlich die Rosen schneiden",
+            priority: "🗲",
+            finished: "",
+            id:2},
+        {title: "einkaufen",
+            issueDate: "12.2.2017",
+            dueDate:  "12.9.2017",
+            description: "Für Fest einen Braten und etwas Feuerwasser." +
+            "Zum Dessert wäre es noch lässig etwas Käse, ach was Eis und ..... wer weiss dass schon so genau. es muss jedenfall genug her",
+            priority: "",
+            finished: "",
+            id:3}]})
+}
+
+function sortNotesOnIssueDate() {
+    console.log('sort Notes on issue date -> give me soul');
+}
+
+function sortNotesOnPriority() {
+    console.log('sort Notes on priority -> give me soul');
+}
