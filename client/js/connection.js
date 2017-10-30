5a6a7b34aeb2768127f15abff292daa354f2425f
heeeman <ckalt@hsr.ch>
2017-10-30 19:17:28 +0100

    Asynch xhr durch fetch ersetzt

--- client/js/connection.js
+++ client/js/connection.js
@@ -3,22 +3,17 @@ var connect = (function () {
     const urlRoot = location.origin + '/notes/';
 
     function loadAll(callBack) {
-        var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function () {
-            if (4 === xhr.readyState) {
-                if (xhr.status === 200) {
-                    callBack(xhr.responseText);
-                }else {
-                    alert('There was a problem with the request. status = ' + xhr.status);
-                }
+        fetch(urlRoot, {
+            method: 'GET', headers: {
+                'Content-Type': 'application/json'
             }
-        };
-        xhr.open('GET', urlRoot, true); //async
-        xhr.setRequestHeader('Accept', 'application/json')
-        xhr.send();
-        return xhr.responseText;
+        }).then(function (res) {
+            // return res.json(); // so liessen sich direkt JS Objekte erzeugen
+            return res.text();
+        }).then(json => {callBack(json);});
     }
 
+    /**synchrone Verbindung**/
     function saveAll(content) {
         var xhr = new XMLHttpRequest();
         xhr.onreadystatechange = function () {
@@ -34,33 +29,59 @@ var connect = (function () {
     }
 
     function putNote(content) {
-        var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function () {
-            if (4 === xhr.readyState) {
-                if (xhr.status !== 200) {
-                    alert('There was a problem with the request. status = ' + xhr.status);
-                }
-            }
-        };
-        xhr.open('PUT', urlRoot, true); //async
-        xhr.setRequestHeader('Content-Type', 'application/json')
-        xhr.send(content);
+
+        fetch(urlRoot, {
+            method: 'PUT', headers: {
+                'Content-Type': 'application/json'
+            },
+            body: content
+        }).then(function (res) {
+            return res.text();
+        });
+
+        /**Variante xhr**/
+        //
+        //
+        // var xhr = new XMLHttpRequest();
+        // xhr.onreadystatechange = function () {
+        //     if (4 === xhr.readyState) {
+        //         if (xhr.status !== 200) {
+        //             alert('There was a problem with the request. status = ' + xhr.status);
+        //         }
+        //     }
+        // };
+        // xhr.open('PUT', urlRoot, true); //async
+        // xhr.setRequestHeader('Content-Type', 'application/json')
+        // xhr.send(content);
     }
 
     function postNote(content, callback) {
-        var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function () {
-            if (4 === xhr.readyState) {
-                if (xhr.status === 200) {
-                    callback(xhr.responseText);
-                }else {
-                    alert('There was a problem with the request. status = ' + xhr.status);
-                }
-            }
-        };
-        xhr.open('POST', urlRoot, true); //async
-        xhr.setRequestHeader('Content-Type', 'application/json')
-        xhr.send(content);
+
+        fetch(urlRoot, {
+            method: 'POST', headers: {
+                'Content-Type': 'application/json'
+            },
+            body: content
+        }).then(function (res) {
+            // return res.json(); // so liessen sich direkt JS Objekte erzeugen
+            return res.text();
+        }).then(json => {callback(json);});
+
+        /**Variante xhr**/
+        //
+        // var xhr = new XMLHttpRequest();
+        // xhr.onreadystatechange = function () {
+        //     if (4 === xhr.readyState) {
+        //         if (xhr.status === 200) {
+        //             callback(xhr.responseText);
+        //         }else {
+        //             alert('There was a problem with the request. status = ' + xhr.status);
+        //         }
+        //     }
+        // };
+        // xhr.open('POST', urlRoot, true); //async
+        // xhr.setRequestHeader('Content-Type', 'application/json')
+        // xhr.send(content);
     }
 
     return {getAll: loadAll,
